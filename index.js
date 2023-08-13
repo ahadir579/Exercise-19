5153c6c04b24b8f6202520573bbaf2118aa18c40
ahadir579 <139805536+ahadir579@users.noreply.github.com>
2023-08-13 23:59:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Importing Guests List from exercise 15
+const Exercise_15_1 = require("../Exercise 15");
+console.log(`\nPrinting message indicating number of guests.`);
+console.log(`We had finally invited ${Exercise_15_1.guestList.length} guest in Exercise 15.`);
